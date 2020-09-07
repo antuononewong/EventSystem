b1b17739fcc405a0951f26d8f2de4f085bb1e37c
Anthony <48633290+antuononewong@users.noreply.github.com>
2020-09-06 22:59:42 -0500

    Update ObjectEventManager.cs
    - Added comments for clarity
    - Removed unnecessary imports
    - Removed debug checker

--- ObjectEventManager.cs
+++ ObjectEventManager.cs
@@ -1,33 +1,36 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
-using UnityEngine.UIElements;
 
+/* Event definitions for different user inputs. Handles pinging
+ * main event handler for said inputs.
+ */
 public class ObjectEventManager : MonoBehaviour
 {
+    // Events
     public static event Action<GameObject> OnObjectSelect;
     public static event Action<GameObject> OnObjectDeselect;
     public static event Action<string> OnAxisDrag;
 
     private GameObject currentObject;
     
-
-    public void Update()
+    // Update is called every frame
+    private void Update()
     {
-        if (Input.GetButtonDown("LeftClick"))
+        if (Input.GetButtonDown("LeftClick")) // Single click
         {
             CheckObjectCollision();
         }
 
-        if (Input.GetButton("LeftClick"))
+        if (Input.GetButton("LeftClick")) // Mouse button held down
         {
             CheckAxisCollision();
         }
      
     }
 
+    // Draws an invisible line from mouse cursor along Z-axis for specified distance. Depending on if
+    // mouse cursor is over an object, different events are pinged and appropriate event handlers will
+    // be called.
     private void CheckObjectCollision()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -39,7 +42,7 @@ public class ObjectEventManager : MonoBehaviour
             {
                 if (currentObject != null)
                 {
-                    OnObjectDeselect?.Invoke(currentObject); // if another obj already selected, deselect it
+                    OnObjectDeselect?.Invoke(currentObject); // If another obj already selected, deselect it
                 }
 
                 OnObjectSelect?.Invoke(newObject);
@@ -49,7 +52,6 @@ public class ObjectEventManager : MonoBehaviour
         }
         else 
         {
-            //Debug.Log("Didn't click UI");
             OnObjectDeselect?.Invoke(currentObject);
             currentObject = null;
             
@@ -57,6 +59,8 @@ public class ObjectEventManager : MonoBehaviour
 
     }
 
+    // Draws an invisible line from mouse cursor along Z-axis for specified distance. If line "hits" an axis,
+    // event will be pinged and Object.EventSystem.OnAxisDragged() will be called.
     private void CheckAxisCollision()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
